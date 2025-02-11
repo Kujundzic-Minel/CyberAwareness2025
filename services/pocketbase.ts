182b2d96d54429f982aaa79279018e5aaf818bf1
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2025-02-11 16:22:47 +0100

    Init : Changement config pocketbase

--- services/pocketbase.ts
+++ services/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://cyberawareness.api.kujundzic.fr');
 export default pb;
